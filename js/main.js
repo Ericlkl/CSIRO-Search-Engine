8413b2048c98a070cdf7c000805bff3570234f1b
ruby -v <Jbssantias@hotmail.com>
2019-01-28 15:04:41 +1000

    created function to store input values

--- js/main.js
+++ js/main.js
@@ -11,17 +11,44 @@ slider.oninput = function() {
 output.innerHTML = this.value;
 }
 
-//the following stores the search history in cookies
-var today = new Date();
-var expicy = new Date(today.getTime() + 30 * 24 * 3600 * 1000); //plus 30 days. cookies lost after thirty days
+var search = new Array(5);
+//if array value is empty add to first
 
-function setCookie(name, value){ //when user press enter or clicks the search button, function called
-	document.cookie=name + "=" + escape(value) + "; path=/; expires=" + expiry.toGMTString();
-}
 
 function storeValues(form){
-	setCookie("field", form.field1.value);
+	for (var i = 0; i < search.length; i ++){
+		if (typeof search[i] === 'undefined'){
+			search[i] = (form.field.value);
+		} else if (typeof search[search.length] !== 'undefined'){
+			//move values down, push last value out of the array
+			for (int x = search.length - 1; x > 0; x--){
+				search[x] = search[x - 1];
+			}
+			search[0] = (form.field.value);
+		}
+	}
 	return true;
 }
 
-if(field = getCookie("field")) document.myForm.field.value = field;
+
+function clearHistory(array){
+	search = new Array(5);
+}
+
+
+// //the following stores the search history in cookies
+// var today = new Date();
+// var expicy = new Date(today.getTime() + 30 * 24 * 3600 * 1000); //plus 30 days. cookies lost after thirty days
+
+// function setCookie(name, value){ //when user press enter or clicks the search button, function called
+// 	document.cookie=name + "=" + escape(value) + "; path=/; expires=" + expiry.toGMTString();
+// }
+
+function storeValues(form){
+	setCookie("field", form.field.value);
+	return true;
+}
+
+// if(history1 = getCookie("history1")) document.myForm.history1.value = field;
+
+//load history
