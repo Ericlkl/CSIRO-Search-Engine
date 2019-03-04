d9b7ca6409c2742be0449651466c818b995ad8f1
Aaron Tolmaci <aaron.tolmaci@hotmail.com>
2019-03-04 12:18:50 +1000

    notaion added for planned work

--- startBackend.sh
+++ startBackend.sh
@@ -5,10 +5,20 @@
 # Correct absolute path for safely running script anywhere.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+
 # Nonblocking ES start, a shutdown script will be written in the future.
 # For demonstrative purposes clean shutdown is uneccesary at this point.
 $DIR/elasticSearch/bin/elasticsearch &
 
 sleep 5
+
+#TODO: while ES not ready sleep
+#TODO: clear existing ES DB (for easier workflow testing
+
+#TODO: index pdf file
+#TODO: index multiple PDF files
+
+#TODO: repeat last 2 steps for various file types
+
 # Kibana for query testing.
 $DIR/kibana/bin/kibana &
