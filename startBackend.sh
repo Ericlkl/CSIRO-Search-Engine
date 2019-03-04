bfe376bc0fd990e28f74ac7f059e9e9416721b0b
Aaron Tolmaci <aaron.tolmaci@hotmail.com>
2019-03-04 10:51:37 +1000

    Added preliminary startup script for backend.

---
+++ startBackend.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# author: 	Aaron Tolmaci
+# desc:		For now this script only starts elasticsearch, eventually it will run a daemon to watch for file changes and index them accordingly.
+
+# Correct absolute path for safely running script anywhere.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Nonblocking ES start, a shutdown script will be written in the future.
+# For demonstrative purposes clean shutdown is uneccesary at this point.
+$DIR/elasticSearch/bin/elasticsearch &
+
+sleep 5
+# Kibana for query testing.
+$DIR/kibana/bin/kibana &
