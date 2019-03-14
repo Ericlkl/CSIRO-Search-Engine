62506d7e86a07760f1b8b15a16f79e694a15af42
Aaron Tolmaci <aaron.tolmaci@hotmail.com>
2019-03-14 13:53:32 +1000

    cleanup deleted folders/files

--- helperScripts/extractDataFiles.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-declare -a arr=("pdf" "doc" "docx" "txt")
-## now loop through the above array
-for i in "${arr[@]}"
-do
-   echo "done with $i"
-   find -iname "*.$i" -exec mv {} ~/Capstone/sortedData/$i \;
-done
--- kibana/node_modules/@elastic/eui/src/services/predicate/index.js
+++
@@ -1,2 +0,0 @@
-export * from './common_predicates';
-export * from './lodash_predicates';
