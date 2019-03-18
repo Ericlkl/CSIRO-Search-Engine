7e9e554e35a88e080c1fddbdd9209af7caffd8be
Eric Lee <Raidenlkl@gmail.com>
2019-03-18 23:02:23 +1000

    ResultList PropTypes and comment added

--- client/src/Components/ResultList.js
+++ client/src/Components/ResultList.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import {connect} from 'react-redux';
 
 const Post = ({ fileName, tags, informations }) => {
   return (
     <div className="ui segment">
-      <p>{fileName}</p>
+      <a href="#">{fileName}</a>
       {informations.map( (info, index) => <p key={index}>{info}</p>)}
     </div>
   )
 }
 
+// PropTypes Requirement
+Post.propTypes = {
+  fileName: PropTypes.string, // XML File Name
+  tags: PropTypes.array,  // Tags embedded in the XML Files
+  informations: PropTypes.array   // Information saved in to the array to display on the result box
+}
+
 class ResultList extends Component {
 
+  propTypes = {
+    searchResult: PropTypes.object  // Search Result / response from the backend server
+  };
+
   render() {
+    // total = Total result found by elastic search
+    // results = array object that contain all the result about this keyword
     const {total, results} = this.props.searchResult;
     return (
       <div className="result-list">
@@ -20,13 +34,13 @@ class ResultList extends Component {
           <div className="ui secondary segment">
             <p> {total} result found</p>
             {
+              // Generate result box by filling information by one result array item
               results.map(result => 
                 <Post 
                   key={result.fileName}
                   fileName={result.fileName}
                   tags={result.tags}
-                  informations={result.informations}
-                />
+                  informations={result.informations} />
               )
             }
           </div>
@@ -36,6 +50,7 @@ class ResultList extends Component {
   }
 }
 
+// Maping Redux store to this class component
 const mapStateToProps = ({searchResult}) => ({searchResult});
 
 export default connect(mapStateToProps)(ResultList);
