18d2dd11e8efc0a1df51ec7ea0962750a084c06a
Eric Lee <Raidenlkl@gmail.com>
2019-03-29 19:25:31 +1000

    Search button will activate the newindexXml.py before fetching data

--- client/src/actions/resultActions.js
+++ client/src/actions/resultActions.js
@@ -4,17 +4,16 @@ import _ from 'lodash';
 import { FETCH_RESULTS } from './types';
 
 export const fetchResults = keyword => async dispatch => {
-    // const updateRes = await axios.get('http://localhost:3001/data');
-    const res = await axios.get('/main/xml/_search?q='+ keyword);
-
+    const updateRes = await axios.get('http://localhost:3001/data/update');
+    const queryData = await axios.get('/main/xml/_search?q='+ keyword);
     // Result Array contains all the result object
     const payload = {
         results: [],
-        total: res.data.hits.total
+        total: queryData.data.hits.total
     };
 
     // Loop through the hits object
-    res.data.hits.hits.forEach( hit => {
+    queryData.data.hits.hits.forEach( hit => {
         // First Split the text and put every text into array
         // Second Filter out the "" by length === 0
         const informations = _.split(hit._source.text,/\\[a-z]+/)
@@ -26,7 +25,7 @@ export const fetchResults = keyword => async dispatch => {
             informations
         });
     })
-    
+        
     dispatch({
         type: FETCH_RESULTS,
         payload
--- server.js
+++ server.js
@@ -4,10 +4,30 @@ const {PythonShell} = require('python-shell')
 const cors = require('cors');
 const app = express();
 
+const options = {
+    mode: 'text',
+    encoding: `utf8`,
+    scriptPath: './sortedTestData/',
+    pythonOptions: ['-u'],
+    pythonPath: `/usr/local/bin/python3`
+};
+
 app.use(express.static('client/build'));
 app.use(cors());
 
-require('./sortedTestData/dataRoute')(app);
+app.get('/data/update', (req, res) => {
+    console.log("Python Script: NewIndexXML.py is running...");
+
+    PythonShell.run('NewIndexXML.py', options, (err, results) => {
+        if (err) {
+            res.status(401).send({msg: "Server Error!"});
+            throw err;
+        } 
+        console.log("Python Script Excuted Successfully!");
+        console.log('results: %j', results);
+        res.status(200).send({msg: "Success!"})
+    }); 
+})
 
 app.get('*', (req,res) => {
     res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'))     
