ad6f28793be03b3e30410b8d9f729546f089cc7e
Eric Lee <Raidenlkl@gmail.com>
2019-04-11 23:54:46 +1000

    Can Fetch the data from Node Server like before but more stable

--- client/src/actions/resultActions.js
+++ client/src/actions/resultActions.js
@@ -1,31 +1,11 @@
 import {nodeServer} from '../api/index';
-import _ from 'lodash';
-
 import { FETCH_RESULTS } from './types';
 
 export const fetchResults = keyword => async dispatch => {
-    const queryData = await nodeServer.post('/data', {keyword} );
-    // Result Array contains all the result object
-    const payload = {
-        results: queryData.data.hits.hits.map( hit => {
-            // Loop through the hits object
-            // First Split the text and put every text into array
-            // Second Filter out the "" by length === 0
-            const informations = _.split(hit._source.text,/\\[a-z]+/)
-                            .filter(word => word.length > 0 );
-                            
-            return {
-                fileName: hit._id,
-                tags: hit._source.tags,
-                informations
-            }
-        }),
-        total: queryData.data.hits.total
-    };
-
-        
+    const res = await nodeServer.post('/data', {keyword} );
+    console.log(res);
     dispatch({
         type: FETCH_RESULTS,
-        payload
+        payload: res.data
     });
 }
--- routes/data.js
+++ routes/data.js
@@ -1,13 +1,14 @@
 var elasticsearch = require('elasticsearch');
 var esclient = new elasticsearch.Client({ host: 'localhost:9200' });
+const _ = require("lodash");
 
 module.exports = app => {
 
-    app.post('/data',  (req,res) => {
+    app.post('/data', async (req,res) => {
         const {keyword} = req.body;
         console.log(keyword)
 
-        esclient.search({
+        const ESresult = await esclient.search({
             index: 'main',
             body: {
               query: {
@@ -25,9 +26,23 @@ module.exports = app => {
               }
             }   // End of the body
         })
-        .then(function (response) {
-              var hits = response.hits.hits;
-              res.send(response);
-        });
+
+        const result = {
+            results: ESresult.hits.hits.map( hit => {
+                // Loop through the hits object
+                // First Split the text and put every text into array
+                // Second Filter out the "" by length === 0
+                const informations = _.split(hit._source.text,/\\[a-z]+/)
+                    .filter(word => word.length > 0 );
+                                
+                return {
+                    fileName: hit._id,
+                    tags: hit._source.tags,
+                    informations
+                }
+            }),
+            total: ESresult.hits.total
+        };
+        res.status(200).send(result);
     })
 }
