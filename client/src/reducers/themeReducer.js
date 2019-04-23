4a10c36d0ea6983627e4333b35d40faf9da431ac
Eric Lee <Raidenlkl@gmail.com>
2019-04-24 01:52:07 +1000

    Cookies for theme added

--- client/src/reducers/themeReducer.js
+++ client/src/reducers/themeReducer.js
@@ -1,4 +1,5 @@
 import { TURN_DARK_MODE_ON, USE_DEFAULT_THEME} from '../actions/types'
+import {bake_cookie, read_cookie} from 'sfcookies'
 
 const defaultTheme = {
     navbarBgColor: "rgb(53, 189, 178)",
@@ -26,10 +27,21 @@ const darkModeTheme = {
 }
 
 export default (state = defaultTheme, action) => {
+
+    // If there is no theme cookies in the browser
+    if (read_cookie("theme").length === 0){
+        bake_cookie("theme", defaultTheme);
+    }
+    else {
+        state = read_cookie("theme");
+    }
+    // state = read_cookie("theme")
     switch(action.type){
         case TURN_DARK_MODE_ON:
+            bake_cookie("theme", darkModeTheme);
             return darkModeTheme;
         case USE_DEFAULT_THEME:
+            bake_cookie("theme", defaultTheme);
             return defaultTheme;
         default:
             return state;
